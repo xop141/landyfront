dae12a1a2afdc4df4193073b77010583238282ed
xop141 <lowsens400@gmail.com>
2025-08-28 21:01:53 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#05000C] border-[#3201F4] text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#121212]text-white`}
       >
            <CafeProvider>
 {children}
