4e8548d420c5b6640e617faab6180cfb197cdf69
xop141 <lowsens400@gmail.com>
2025-10-13 14:57:30 +0800

    sda

--- src/app/partner/[id]/page.jsx
+++ src/app/partner/[id]/page.jsx
@@ -35,7 +35,7 @@ const Page = () => {
 
   return (
     <div className="p-4">
-      {/* <button
+      <button
         className="bg-blue-500 text-white px-4 py-2 mb-4 rounded"
         onClick={test}
       >
@@ -58,7 +58,7 @@ const Page = () => {
             </p>
           </div>
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };
