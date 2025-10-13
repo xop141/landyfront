ea5cd09e652a1de3861ee4a2a066f9ab37d35ab8
xop141 <lowsens400@gmail.com>
2025-10-13 13:06:41 +0800

    test1

--- src/app/partner/[id]/page.jsx
+++ src/app/partner/[id]/page.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client";
+import React from "react";
+import axios from "axios";
 
-const page = () => {
+const Page = () => {
+  const test = async () => {
+    try {
+      const response = await axios.post("https://landybackend.onrender.com/pc/member");
+      console.log("Data from backend:", response.data);
+    } catch (err) {
+      console.error("Error fetching members:", err.response?.data || err.message);
+    }
+  };
+
   return (
     <div>
-      
+      <button
+        className="bg-green-300 px-[20px] py-[10px]"
+        onClick={test}
+      >
+        sad
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
